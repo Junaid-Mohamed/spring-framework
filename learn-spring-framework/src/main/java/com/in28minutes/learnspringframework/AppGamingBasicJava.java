4fcd68a416471e358b3528ff097a50d3ac43274a
JunaidMohamed <JunaidMohamed@DESKTOP-IN7RAVL>
2024-01-17 11:06:05 +0530

    created a GamingConsole class and introduced loosely coupled concept

--- learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/AppGamingBasicJava.java
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/AppGamingBasicJava.java
@@ -2,6 +2,7 @@ package com.in28minutes.learnspringframework;
 
 import com.in28minutes.learnspringframework.game.GameRunner;
 import com.in28minutes.learnspringframework.game.MarioGame;
+import com.in28minutes.learnspringframework.game.PackmanGame;
 import com.in28minutes.learnspringframework.game.SuperContraGame;
 
 public class AppGamingBasicJava {
@@ -10,7 +11,9 @@ public class AppGamingBasicJava {
 		// TODO Auto-generated method stub
 //		var marioGame = new MarioGame();
 		var superContraGame = new SuperContraGame();
-		var gameRunner = new GameRunner(superContraGame);
+		var game = new MarioGame();
+		var packmanGame = new PackmanGame();
+		var gameRunner = new GameRunner(packmanGame);
 		gameRunner.run();
 	}
 
