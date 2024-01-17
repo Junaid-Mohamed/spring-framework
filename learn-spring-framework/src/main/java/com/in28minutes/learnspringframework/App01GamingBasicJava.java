b33611f9d1b3d3f318055cea51914a4a5f76cdea
JunaidMohamed <JunaidMohamed@DESKTOP-IN7RAVL>
2024-01-17 14:28:35 +0530

    initiated Java spring and understanding of Spring context

--- learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/App01GamingBasicJava.java
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/App01GamingBasicJava.java
@@ -5,15 +5,17 @@ import com.in28minutes.learnspringframework.game.MarioGame;
 import com.in28minutes.learnspringframework.game.PackmanGame;
 import com.in28minutes.learnspringframework.game.SuperContraGame;
 
-public class AppGamingBasicJava {
+public class App01GamingBasicJava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 //		var marioGame = new MarioGame();
 		var superContraGame = new SuperContraGame();
 		var game = new MarioGame();
-		var packmanGame = new PackmanGame();
+		var packmanGame = new PackmanGame(); //1. Object creation
 		var gameRunner = new GameRunner(packmanGame);
+		//2: Object creation + wiring of dependencies
+		// packmanGame is dependency for GameRunner
 		gameRunner.run();
 	}
 
