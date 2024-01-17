9409f8112d39f94d871bc73da32d43a74e6799e9
JunaidMohamed <JunaidMohamed@DESKTOP-IN7RAVL>
2024-01-17 15:55:41 +0530

    game class Spring setup

---
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/helloworld/App02HelloWorldSpring.java
@@ -0,0 +1,45 @@
+package com.in28minutes.learnspringframework.helloworld;
+
+import java.util.Arrays;
+
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.in28minutes.learnspringframework.game.GameRunner;
+import com.in28minutes.learnspringframework.game.MarioGame;
+import com.in28minutes.learnspringframework.game.PackmanGame;
+import com.in28minutes.learnspringframework.game.SuperContraGame;
+
+public class App02HelloWorldSpring {
+
+	public static void main(String[] args) {
+	//1. Launch a spring context / (spring container) -
+		
+//	usual practise is to put context into try catch and in finally you close the context.
+//		but with new feature, try with resources you put into try block if any exception the context.close call off automatically.
+		try(var context = new	AnnotationConfigApplicationContext(HelloWorldConfiguration.class);){
+			//2. Configure the things we want spring to manage - @Configuration
+			//we did this in HelloWorldConfiguration class
+			// name - is a @bean here
+			
+			// 3. Retrieved beans managed by Spring
+				System.out.println(context.getBean("name")); // pass in the same name as method name in configuration class
+				System.out.println(context.getBean("age"));
+				System.out.println(context.getBean("person"));
+				System.out.println(context.getBean("address"));
+				System.out.println(context.getBean("person2MethodCall"));
+				System.out.println(context.getBean("person3Parameters"));
+				
+				System.out.println(context.getBean(Adress.class));
+				System.out.println(context.getBean("person4Parameters"));
+				
+//				System.out.println("-----------------------------------------------------------\n Bean names \n");
+//				Arrays.stream(context.getBeanDefinitionNames())
+//					.forEach(System.out::println);
+		}
+	
+		
+
+	}
+
+}
